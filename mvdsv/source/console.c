082896d23c3e82af7d3d29c62d9c5a9b25129d63
VVD <vvd@pochta.ru>
2005-07-05 13:04:42 +0000

    bugfix: format string vulnerabilities (by droogie)

--- mvdsv/source/console.c
+++ mvdsv/source/console.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: console.c,v 1.2 2005/05/27 15:09:48 vvd0 Exp $
+	$Id: console.c,v 1.3 2005/07/05 13:03:49 vvd0 Exp $
 */
 // console.c
 
@@ -693,7 +693,7 @@ void Con_NotifyBox (char *text)
 // during startup for sound / cd warnings
 	Con_Printf("\n\n\35\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\37\n");
 
-	Con_Printf (text);
+	Con_Printf ("%s", text);
 
 	Con_Printf ("Press a key.\n");
 	Con_Printf("\35\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\37\n");
