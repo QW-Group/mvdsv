914ad6b4e20118114c690ee2cff02f6c78f46598
VVD <vvd@pochta.ru>
2006-02-28 14:03:22 +0000

    net_wins.c renamed to net.c and optimized network inicialization

--- mvdsv/source/net.c
+++ mvdsv/source/net.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: net_wins.c,v 1.12 2006/02/28 14:03:22 vvd0 Exp $
+	$Id: net.c,v 1.1 2006/02/28 14:03:22 vvd0 Exp $
 */
 // net_wins.c
 
