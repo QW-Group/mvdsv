edc7a5f6b787f6e936e103270a4579041a47b419
disconnect <disconnect@qw-dev.net>
2006-04-24 20:50:55 +0000

    small fix

--- mvdsv/source/sv_sys_win.c
+++ mvdsv/source/sv_sys_win.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  
-	$Id: sv_sys_win.c,v 1.24 2006/04/12 17:08:57 vvd0 Exp $
+	$Id: sv_sys_win.c,v 1.25 2006/04/24 20:50:55 disconn3ct Exp $
 */
 
 #include <conio.h>
@@ -678,8 +678,7 @@ __inline void Sys_Telnet (void)
 	static double		cur_time_not_auth;
 	if (telnet_connected)
 	{
-		if ((tempsock =
-					accept (net_telnetsocket, (struct sockaddr*)&remoteaddr_temp, &sockaddr_len)) > 0)
+		if ((tempsock = accept (net_telnetsocket, (struct sockaddr*)&remoteaddr_temp, (socklen_t *) &sockaddr_len)) > 0)
 		{
 			//			if (remoteaddr_temp.sin_addr.s_addr == inet_addr ("127.0.0.1"))
 			send (tempsock, "Console busy by another user.\n", 31, 0);
@@ -700,8 +699,7 @@ __inline void Sys_Telnet (void)
 	}
 	else
 	{
-		if ((telnet_iosock =
-					accept (net_telnetsocket, (struct sockaddr*)&remoteaddr, &sockaddr_len)) > 0)
+		if ((telnet_iosock = accept (net_telnetsocket, (struct sockaddr*)&remoteaddr, (socklen_t *)&sockaddr_len)) > 0)
 		{
 			//			if (remoteaddr.sin_addr.s_addr == inet_addr ("127.0.0.1"))
 			//			{
