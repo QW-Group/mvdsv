1c1ec5237ad5dfc2c7c0d34ada65fe48d7871529
VVD <vvd@pochta.ru>
2006-06-02 15:07:02 +0000

    increase version to 0.19.24-CVS & added initial support of NetBSD

--- mvdsv/source/version.h
+++ mvdsv/source/version.h
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-	$Id: version.h,v 1.25 2006/05/26 15:28:30 disconn3ct Exp $
+	$Id: version.h,v 1.26 2006/06/02 15:07:02 vvd0 Exp $
 */
 // version.h
 
@@ -39,6 +39,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 /* <---  */
 
+#ifdef __NetBSD__
+#define QW_PLATFORM			"NetBSD"
+#define QW_PLATFORM_SHORT	"(n)"
+#endif
+
 #ifdef __linux__
 #define QW_PLATFORM			"Linux"
 #define QW_PLATFORM_SHORT	"(l)"
@@ -55,8 +60,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #define	QW_VERSION			"2.40"
-#define QWE_VERSION			"0.19.23-CVS"
-#define QWE_VERNUM			0.1923
+#define QWE_VERSION			"0.19.24-CVS"
+#define QWE_VERNUM			0.1924
 #define SERVER_NAME			"MVDSV"
 #define QWDTOOLS_NAME		"QWDtools"
 #define PROJECT_NAME		SERVER_NAME
