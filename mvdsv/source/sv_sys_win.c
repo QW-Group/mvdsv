e6a7e0bf15b3ba85264f73a75e268c5bfcfd7251
VVD <vvd@pochta.ru>
2006-08-07 13:03:59 +0000

    - improved build number calculation - bugfix: "99% downloaded" - sometimes after downloading client don't rename temporary file to real filename and don't hide download bar - show 99% (EXPERIMENTAL) (Thanks to Renzo) - version was increased from 0.20.03-CVS to 0.20.04-CVS

--- mvdsv/source/sv_sys_win.c
+++ mvdsv/source/sv_sys_win.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  
-	$Id: sv_sys_win.c,v 1.32 2006/06/19 16:46:16 vvd0 Exp $
+	$Id: sv_sys_win.c,v 1.33 2006/08/07 13:03:59 vvd0 Exp $
 */
 
 #include "qwsvdef.h"
