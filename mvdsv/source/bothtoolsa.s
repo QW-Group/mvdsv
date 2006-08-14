4840f0ab48f765135e3f40f033202e54a68697b2
VVD <vvd@pochta.ru>
2006-08-14 15:37:28 +0000

    - optimized detection of ktpro and added detection of ktpro versions from 1.50 to 1.57 - big cleanup - removed worlda.s - client only file - version was increased from 0.20.04-CVS to 0.20.05-CVS

---
+++ mvdsv/source/bothtoolsa.s
@@ -0,0 +1,58 @@
+/*
+	bothtools.s
+
+	x86 assembly language math routines
+
+	Copyright (C) 2006 VVD (vvd0@sorceforge.net).
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+
+	$Id: bothtoolsa.s,v 1.1 2006/08/14 15:37:28 vvd0 Exp $
+*/
+
+#include "asm_i386.h"
+#include "quakeasm.h"
+
+
+#ifdef id386
+
+	.text
+
+#define	val	4
+
+.globl C(ShortSwap)
+C(ShortSwap):
+
+	movzwl	val(%esp),%eax
+	xchgb	%al,%ah
+	ret
+
+
+.globl C(LongSwap)
+C(LongSwap):
+
+.globl C(FloatSwap)
+C(FloatSwap):
+
+	movl	val(%esp),%eax
+	bswap	%eax
+	ret
+
+
+#endif	/* id386 */
