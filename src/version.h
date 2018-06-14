290fbf5180abf6bdd04cd42f68e4832fadffa83b
meag <meag@acm.org>
2018-06-14 06:29:48 +0100

    VERSION: 0.32 release candidate 1

--- src/version.h
+++ src/version.h
@@ -61,16 +61,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 
-#define	QW_VERSION			"2.40"
-#define VERSION_NUMBER		"0.32-dev.1"
-#define VERSION_NUM			0.32
-#define SERVER_NAME			"MVDSV"
+#define QW_VERSION          "2.40"
+#define VERSION_NUMBER      "0.32-rc1"
+#define VERSION_NUM         0.32
+#define SERVER_NAME         "MVDSV"
 
-#define PROJECT_NAME		SERVER_NAME
-#define PROJECT_URL			"https://github.com/deurk/mvdsv"
-#define BUILD_DATE			"Build date: " __DATE__ ", " __TIME__
+#define PROJECT_NAME        SERVER_NAME
+#define PROJECT_URL         "https://github.com/deurk/mvdsv"
+#define BUILD_DATE          "Build date: " __DATE__ ", " __TIME__
 
-#define QWDTOOLS_NAME		"QWDtools"
+#define QWDTOOLS_NAME       "QWDtools"
 
 char *VersionString (void);
 char *VersionStringFull (void);
